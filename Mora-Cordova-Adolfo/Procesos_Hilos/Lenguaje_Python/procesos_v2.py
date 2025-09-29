c4c069216ac74484f73fc410b047a0354fb3b6e3
amorac4 <amorac2024@cic.ipn.mx>
2025-09-29 13:47:31 -0600

    modified:   Mora-Cordova-Adolfo/Procesos_Hilos/Lenguaje_Python/procesos_v2.py

--- Mora-Cordova-Adolfo/Procesos_Hilos/Lenguaje_Python/procesos_v2.py
+++ Mora-Cordova-Adolfo/Procesos_Hilos/Lenguaje_Python/procesos_v2.py
@@ -1,37 +1,27 @@
 import os
 import sys 
-import time
 
-def hijo(i):
+def hijo():
+    print(f"Soy el proceso hijo con PID {os.getpid()}")
 
-    print(f"Soy el proceso hijo {i} con PID {os.getpid()}")
-
-def padre(i, pid_hijo):
-    print(f"Soy el proceso padre {i} con PID {os.getpid()} y mi hijo tiene PID {pid_hijo}")
+def padre(pid_hijo):
+    print(f"Soy el proceso padre con PID {os.getpid()} y mi hijo tiene PID {pid_hijo}")
 
 def main():
-    hijos = []
-    for i in range(1,4):
-        try:
-             pid  = os.fork()
-        except OSError as e:
-             pid = -1
-        match pid:
-                case -1:
-                    print("Error al crear el proceso")
-                    sys.exit(1)
-                case 0:
-                    hijo(i)
-                    sys.exit(0)
-                case _:
-                    hijos.append(pid)
-                    padre(i, pid)
-    for pid in hijos:
-        try :
-                os.waitpid(pid, 0)  
-        except ChildProcessError:
-                pass
+    try:
+        pid = os.fork()
+    except OSError as e:
+        pid = -1
+    
+    match pid:
+        case -1:
+            print("Error al crear el proceso")
+            sys.exit(1)
+        case 0:
+            hijo()
+            sys.exit(0)
+        case _:
+            padre(pid)
 
 if __name__ == "__main__":
-        main()
-             
+    main()
